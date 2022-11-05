798eb4daa0b583e0162b34493ce7b26967d08880
Lucas Alves <lucas.shoppz@gmail.com>
2022-11-04 21:41:09 -0300

    newsletter

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        azul: {
+          claro: '#C5DFFF',
+          escuro: '#061E3C',
+          hover: '#1057B0'
+        },
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
